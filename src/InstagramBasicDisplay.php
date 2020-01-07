860fc4d0dd03fbfe94fb8c0552aadec0f8be13f5
Herman Schutte <herman.schutte@gmail.com>
2020-01-07 14:50:51 +0200

    Add Instagram class

---
+++ src/InstagramBasicDisplay.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EspressoDev\InstagramBasicDisplay;
+
+class InstagramBasicDisplay
+{
+    public function __construct() 
+    {
+
+    }
+
+    public function testMe()
+    {
+        return 'hello world!!!!!';
+    }
+}
