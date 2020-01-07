9bb745302dc65d1b4230e236c9e5d7c375f1a68d
Herman Schutte <herman.schutte@gmail.com>
2020-01-07 13:52:04 +0200

    Add Instagram class

---
+++ src/Instagram.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EspressoDev\Instagram;
+
+class Instagram 
+{
+    public function __construct() 
+    {
+
+    }
+
+    public function testMe()
+    {
+        return 'hello world!';
+    }
+}
